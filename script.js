742ff8aa44f83d4695800c78fd0b88b7ca6556e6
Koral <koral@Korals-MacBook-Air.local>
2023-08-22 15:45:23 +0300

    initial commit

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 const API_KEY = 'cjiacb9r01qonds7mqhgcjiacb9r01qonds7mqi0'; // Replace with your Finnhub API key
 
+function plotData(dates, prices) {
+    const trace = {
+        type: 'scatter',
+        mode: 'lines',
+        name: 'Stock Price',
+        x: dates,
+        y: prices,
+        line: {color: '#17BECF'}
+    };
+
+    const layout = {
+        title: 'Stock Price Over Time',
+        xaxis: {
+            title: 'Date',
+            type: 'date'
+        },
+        yaxis: {
+            title: 'Price (in USD)'
+        }
+    };
+
+    Plotly.newPlot('results', [trace], layout);
+}
+
 function fetchData() {
     const stockSymbol = document.getElementById('stock').value;
     const startDate = new Date(document.getElementById('startDate').value).getTime() / 1000; // Convert to UNIX timestamp
@@ -10,8 +34,12 @@ function fetchData() {
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            // Display the data (for demonstration purposes, you can format it as you like)
-            document.getElementById('results').innerText = JSON.stringify(data, null, 2);
+            // Assuming the 't' key in the response contains the dates (UNIX timestamps)
+            const dates = data['t'].map(timestamp => new Date(timestamp * 1000).toISOString().split('T')[0]);
+            const prices = data['c'];
+
+            // Plot the data using Plotly.js
+            plotData(dates, prices);
         })
         .catch(error => {
             console.error('Error fetching data:', error);
