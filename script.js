3438718668e6e684baa5437ea8e4152d05056c9c
Koral <koral@Korals-MacBook-Air.local>
2023-08-22 15:19:27 +0300

    initial commit

--- script.js
+++ script.js
@@ -1,54 +1,19 @@
-function plotData(dates, prices) {
-    const trace = {
-        type: 'scatter',
-        mode: 'lines',
-        name: 'Stock Price',
-        x: dates,
-        y: prices,
-        line: {color: '#17BECF'}
-    };
+const API_KEY = 'cjiacb9r01qonds7mqhgcjiacb9r01qonds7mqi0'; // Replace with your Finnhub API key
 
-    const layout = {
-        title: 'Stock Price Over Time',
-        xaxis: {
-            title: 'Date',
-            type: 'date'
-        },
-        yaxis: {
-            title: 'Price (in USD)'
-        }
-    };
+function fetchData() {
+    const stockSymbol = document.getElementById('stock').value;
+    const startDate = new Date(document.getElementById('startDate').value).getTime() / 1000; // Convert to UNIX timestamp
+    const endDate = new Date(document.getElementById('endDate').value).getTime() / 1000; // Convert to UNIX timestamp
 
-    Plotly.newPlot('chartContainer', [trace], layout);
-}
-
-function fetchAndPlotData() {
-    const apiKey = 'PFWM5H94IR01A1HR.';
-    const ticker = document.getElementById('stockTicker').value;
-    const startDate = document.getElementById('startDate').value;
-    const endDate = document.getElementById('endDate').value;
-
-    const url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${ticker}&apikey=${apiKey}`;
+    const url = `https://finnhub.io/api/v1/stock/candle?symbol=${stockSymbol}&resolution=D&from=${startDate}&to=${endDate}&token=${API_KEY}`;
 
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            const timeSeries = data['Time Series (Daily)'];
-            const dates = [];
-            const prices = [];
-
-            for (let date in timeSeries) {
-                if (date >= startDate && date <= endDate) {
-                    dates.push(date);
-                    prices.push(parseFloat(timeSeries[date]['4. close']));
-                }
-            }
-
-            // Plot the data using Plotly.js
-            plotData(dates, prices);
+            // Display the data (for demonstration purposes, you can format it as you like)
+            document.getElementById('results').innerText = JSON.stringify(data, null, 2);
         })
         .catch(error => {
-            console.error("Error fetching stock data:", error);
+            console.error('Error fetching data:', error);
         });
 }
-
